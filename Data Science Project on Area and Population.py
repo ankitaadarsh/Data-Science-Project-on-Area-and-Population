ddbfbc5ea758adf7cd1afda7e75d995bff254a82
ankitaadarsh <ankitaadarsh08@gmail.com>
2022-07-25 00:46:23 +0530

    added a repository

---
+++ Data Science Project on Area and Population.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+cities = pd.read_csv("Random_data_California.csv")
+print(cities.head())
+# extracting the data we ar interested in
+latitude, longitude = cities["latd"], cities["longd"]
+population, area = cities["population_total"], cities["area_total_km2"]
+# to scatter the points, using size and color but without label
+seaborn.set()
+plt.scatter(longitude, latitude, label=None, c=np.log10(population), cmap='viridis', s=area, linewidth=0, alpha=0.5)
+plt.gca(aspect='equal')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.colorbar(label='log$_{10}$(population)')
+plt.clim(3, 7)
+# now we will create a legend, we will plot empty lists with the desired size and label
+for area in [100, 300, 500]:
+    plt.scatter([], [], c='k', alpha=0.3, s=area, label=str(area) + 'km$^2$')
+plt.legend(scatterpoints=1, frameon=False, labelspacing=1, title='City Areas')
+plt.title("Area and Population of California Cities")
+plt.show()
